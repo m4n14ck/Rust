032e187a4ada229a653cd05fcfa46c790ec3c5fa
m4n14ck <103155355+m4n14ck@users.noreply.github.com>
2026-07-03 23:28:34 -0600

    Add files via upload

---
+++ curso_rust/1_hola_mundo.rs
@@ -0,0 +1,9 @@
+fn main(){
+    /* 
+    En rust, usamos la macro println!() para imprimir la salida
+    en la consola. Ten en cuenta que println! termina con ln
+    una letra l minuscula(como en line), no una L mayuscula
+    */
+
+    println!("Hola mundo");
+}
---
+++ curso_rust/2_comentarios.rs
@@ -0,0 +1,28 @@
+fn main(){
+
+    /* 
+    Los comentarios son notas que escribes dentro de tu codigo.
+    El compilador los ignora por completo; existen solo para
+    ayudar a los humanos a entender codigo.
+
+    Para escribir un comentario de una sola linea, usa //
+    Todo lo que este despues de // hasta el final de la linea
+    es ignrado.
+    */
+    
+    // Este es un comentario
+    println!("Hola mundo");
+
+    // Un comentario tambien se puede escribir al final de una linea,
+    // despues del codigo
+    println!("Hola mundo"); // Esto imprime Hola mundo
+
+    // Para comentarios que abarcan varias lineas, usa /* para
+    // comenzar y */ para terminar 
+
+    /* Este es un comentario de varias lineas.
+       El compilador lo ignora todo */
+    println!("Bienvenido");
+
+    
+}
