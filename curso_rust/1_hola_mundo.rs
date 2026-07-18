d6522c3377a641231703e03d352deac4f1f8bc02
m4n14ck <103155355+m4n14ck@users.noreply.github.com>
2026-07-17 20:12:27 -0600

    Delete curso_rust/1_hola_mundo.rs

--- curso_rust/1_hola_mundo.rs
+++
@@ -1,9 +0,0 @@
-fn main(){
-    /* 
-    En rust, usamos la macro println!() para imprimir la salida
-    en la consola. Ten en cuenta que println! termina con ln
-    una letra l minuscula(como en line), no una L mayuscula
-    */
-
-    println!("Hola mundo");
-}
